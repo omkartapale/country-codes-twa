e715e1d26d7ce55305a6517b84f5a4d25aacbc83
Omkar Tapale <omkartapale@gmail.com>
2025-08-24 15:48:31 +0530

    Revive old stage app

---
+++ app/build/generated/source/buildConfig/release/app/vercel/country_codes/twa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.vercel.country_codes.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "app.vercel.country_codes.twa";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2";
+}
